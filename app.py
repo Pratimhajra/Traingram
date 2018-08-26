0e87e60d71b8e24dedfdfbf8461b2b47d9797bee
Kanishk Singh <singh.kanishk07@gmail.com>
2018-08-26 18:41:03 +0530

    DEBUGGING attempt 5

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ DEVELOPER_ACCESS_TOKEN = os.getenv("DEVELOPER_ACCESS_TOKEN")
 def webhook():
     
     req = request.get_json(silent=True, force=True)
-    print("Request:", req)
     
     getIntent = req.get("queryResult").get("intent").get("displayName")
     if(getIntent == "LIVE_STATUS"):
@@ -38,7 +37,64 @@ def webhook():
         print("Message: ", message)
     #elif(getIntent == "TRAINS_BETWEEN_STATIONS"):
 
-    my_result =  {
+    my_result = {
+  "fulfillmentText": message,
+  "fulfillmentMessages": [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://assistant.google.com/"
+          }
+        ]
+      }
+    }
+  ],
+  "source": "https://traingram.herokuapp.com/",
+  "payload": {
+    "google": {
+      "expectUserResponse": "true",
+      "richResponse": {
+        "items": [
+          {
+            "simpleResponse": {
+              "textToSpeech": message
+            }
+          }
+        ]
+      }
+    },
+    "facebook": {
+      "text": "Hello, Facebook!"
+    },
+    "slack": {
+      "text": "This is a text response for Slack."
+    }
+  },
+  "outputContexts": [
+    {
+      "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+      "lifespanCount": 5,
+      "parameters": {
+        "param": "param value"
+      }
+    }
+  ],
+  "followupEventInput": {
+    "name": "event name",
+    "languageCode": "en-US",
+    "parameters": {
+      "param": "param value"
+    }
+  }
+}
+
+
+    """my_result =  {
         "fulfillmentText": message,
         "source": "Traingram-Dialogflow-Webhook",
         "payload": {
@@ -49,6 +105,7 @@ def webhook():
         "status":200,
         "errorType":"success"
     }
+    """
     res = json.dumps(my_result, indent=4)
     r = make_response(res)
     r.headers['Authorization'] = 'Bearer ' + DEVELOPER_ACCESS_TOKEN
