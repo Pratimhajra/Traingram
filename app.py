4573befe1126a2165865fc55131ddfff9acd4749
Kanishk Singh <singh.kanishk07@gmail.com>
2018-08-29 09:34:31 +0530

    railrider test

--- app.py
+++ app.py
@@ -2,57 +2,53 @@
 
 # Not ready!
 # Finish NTES.py functions before moving to this part.
-
+import time
 from flask import Flask
 from flask import request	
-from flask import make_response
+from flask import make_response, jsonify
 
 import fileinput
 import sys
 
 import os.path
 import requests
+from requests.exceptions import ReadTimeout, ConnectTimeout
 import json
 
 #from NTES import live_status
-from NTES import *
-
+#from NTES import *
+from test2 import *
 # Move NTES.py to flask-webhook/ after completing NTES functions
 
 
 # Flask app should start in global layout
 app = Flask(__name__)
+#DEVELOPER_ACCESS_TOKEN = os.getenv("DEVELOPER_ACCESS_TOKEN")
+#session = requests.Session()
+#session.get("https://enquiry.indianrail.gov.in/ntes/IamAlive")
+
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-    
+    start_time = time.time()
     req = request.get_json(silent=True, force=True)
     #print("Request:")
     
     getIntent = req.get("queryResult").get("intent").get("displayName")
     if(getIntent == "LIVE_STATUS"):
         getParams = req.get("queryResult").get("parameters")
-        TrainNo = getParams.get("trainNumber")[0] # trainNumber returned as a list
+        TrainNo = getParams.get("trainNumber") # trainNumber returned as a list
         StnName = getParams.get("stnName")
         getQuery = req.get("queryResult").get("queryText")
+
         print(getQuery, "\n", TrainNo, "\n", StnName)
         message = live_status(TrainNo, StnName)
     #elif(getIntent == "TRAINS_BETWEEN_STATIONS"):
 
-    my_result =  {
-        "fulfillmentText": message,
-        "source": "Traingram-Dialogflow-Webhook",
-        "payload": {
-            "google":{
-                "textToSpeech": message
-            }
-        }
-        "status":200,
-        "errorType":"success"
-    }
-    res = json.dumps(my_result, indent=4)
-    r = make_response(res)
+
+    r = make_response((jsonify({'fulfillmentText': message})))
+    #r.headers['Authorization'] = 'Bearer ' + DEVELOPER_ACCESS_TOKEN
     r.headers['Content-Type'] = 'application/json'
     return r
     
