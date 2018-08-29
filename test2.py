9629b964c742000c3e1876fef435f91e753cec71
Kanishk Singh <singh.kanishk07@gmail.com>
2018-08-29 09:54:50 +0530

    delay_arr

--- test2.py
+++ test2.py
@@ -7,9 +7,11 @@ def live_status(TrainNo, stnCode):
 	date=today.strftime("%d-%b-%y")
 	response=requests.get(f"https://api.railrider.in/api_rr_v3_test.php?page_type=live_train_status&train_num={TrainNo}&journey_station={stnCode}&journey_date={date}")
 	data=response.json()
-	var=data.get('delayArr')
+	var=data.get('delay_arr')
 	print(var)
 #def trains_btwn_stations(stn1, stn2, viaStn="null", trainType="ALL"):
 
 if __name__ == '__main__':
 	live_status(19016,'PLG')
+
+	https://api.railrider.in/api_rr_v3_test.php?page_type=live_train_status&train_num=19016&journey_station=PLG&journey_date=29-Aug-2018
