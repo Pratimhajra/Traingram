f18558dc181b0b9eec1d124f6b0710205216baff
Unknown <mihir.prashant.kulkarni@gmail.com>
2018-08-29 09:21:09 +0530

    test2

---
+++ test2.py
@@ -0,0 +1,14 @@
+import requests
+import time
+import datetime
+import json
+def live_status(TrainNo, stnCode):
+	today=datetime.datetime.now()
+	date=today.strftime("%d-%b-%y")
+	response=requests.get(f"https://api.railrider.in/api_rr_v3_test.php?page_type=live_train_status&train_num={TrainNo}&journey_station={stnCode}&journey_date={date}")
+	data=response.json()
+	var=data['delay_arr']
+	print(var)
+#def trains_btwn_stations(stn1, stn2, viaStn="null", trainType="ALL"):
+
+live_status(19016,'PLG')
