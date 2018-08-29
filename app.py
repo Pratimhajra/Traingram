14b121ced1540d2aca2269f8f2789e5a64a6377f
Kanishk Singh <ArionMiles@users.noreply.github.com>
2018-08-29 18:25:46 +0530

    Migrated from NTES to RailRider & Webhook response fixes (#14)
    * corrected json
    
    * Added authorization
    
    * Migrated from NTES to RailRider & Webhook response fixes

--- app.py
+++ app.py
@@ -1,58 +1,34 @@
 # coding: utf-8
-
-# Not ready!
-# Finish NTES.py functions before moving to this part.
+# Work in Progress
 
 from flask import Flask
 from flask import request	
-from flask import make_response
+from flask import make_response, jsonify
 
-import fileinput
-import sys
+import os
 
-import os.path
-import requests
-import json
-
-#from NTES import live_status
-from NTES import *
-
-# Move NTES.py to flask-webhook/ after completing NTES functions
-
+from RailRider import *
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
+DEVELOPER_ACCESS_TOKEN = os.getenv("DEVELOPER_ACCESS_TOKEN")
 
 @app.route('/', methods=['POST'])
 def webhook():
     
     req = request.get_json(silent=True, force=True)
-    #print("Request:")
     
     getIntent = req.get("queryResult").get("intent").get("displayName")
     if(getIntent == "LIVE_STATUS"):
         getParams = req.get("queryResult").get("parameters")
-        TrainNo = getParams.get("trainNumber")[0] # trainNumber returned as a list
+        TrainNo = int(getParams.get("trainNumber"))
         StnName = getParams.get("stnName")
         getQuery = req.get("queryResult").get("queryText")
-        print(getQuery, "\n", TrainNo, "\n", StnName)
         message = live_status(TrainNo, StnName)
     #elif(getIntent == "TRAINS_BETWEEN_STATIONS"):
 
-    my_result =  {
-        "fulfillmentText": message,
-        "source": "Traingram-Dialogflow-Webhook",
-        "payload": {
-            "google":{
-                "textToSpeech": message
-            }
-        }
-        "status":200,
-        "errorType":"success"
-    }
-    res = json.dumps(my_result, indent=4)
-    r = make_response(res)
+    r = make_response((jsonify({'fulfillmentText': message})))
+    r.headers['Authorization'] = 'Bearer ' + DEVELOPER_ACCESS_TOKEN
     r.headers['Content-Type'] = 'application/json'
     return r
     
@@ -62,4 +38,4 @@ if __name__ == '__main__':
 
 	print("Starting app on port %d" % port)
 
-	app.run(debug=True, port=port, host='0.0.0.0')
+	app.run(debug=False, port=port, host='0.0.0.0')
