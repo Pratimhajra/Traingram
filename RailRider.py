35e36eb6fe093dd787b12ea7bb60eedd7f5a78c7
Unknown <pranavgharat1999@gmail.com>
2018-08-30 15:32:25 +0530

    Add PNR Status

--- RailRider.py
+++ RailRider.py
@@ -2,7 +2,7 @@ import json
 import requests
 import time
 import datetime
-
+from json.decoder import JSONDecodeError
 
 def live_status(TrainNo, stnName):
     stnCode = stnName_to_stnCode(stnName)
@@ -17,16 +17,23 @@ def live_status(TrainNo, stnName):
 def PNR_status(pnr):
     payload = {'pnr_post':pnr}
     response=requests.post("https://www.railrider.in/api/ajax_pnr_check.php", data=payload)
-    data = response.text
-    jsond = json.loads(data)
-    doj = jsond.get('doj')
-    trainname=jsond.get('train_name')
-    clas = jsond.get('class1')
-    total_passengers = jsond.get('total_passengers')
-    from_station = jsond.get('from_station').get('name')
-    to_station = jsond.get('to_station').get('name')
-    total_fare = jsond.get('total_fare')
-    message = "The PNR status is:\n"+"Date of journey: "+doj+"\nTrain_name: "+trainname+"\nclass: "+clas+"\nTotal_passengers: "+total_passengers+"\nFrom_station:"+from_station+"\n To_station: "+to_station#+"\nTotal_fare: "+total_fare
+    count = 1
+    if(count<=2):
+        try:
+            data = response.json()
+        except JSONDecodeError:
+            print("JSONDecodeError... Retrying... Attempt: ", count)
+            data = response.json()
+    #data = response.json() #json.loads(data)
+    doj = data.get('doj')
+    trainname=data.get('train_name')
+    clas = data.get('class1')
+    total_passengers = data.get('total_passengers')
+    from_station = data.get('from_station').get('name')
+    to_station = data.get('to_station').get('name')
+    total_fare = str(data.get('total_fare'))
+    #message = "The PNR status is:\n"+"Date of journey: "+doj+"\nTrain_name: "+trainname+"\nclass: "+clas+"\nTotal_passengers: "+total_passengers+"\nFrom_station:"+from_station+"\n To_station: "+to_station#+"\nTotal_fare: "+total_fFRare
+    message = f"The PNR status is:\nDate of journey: {doj}\nTrain Name: {trainname}\nClass: {clas}\nTotal Passengers: {total_passengers}\nFrom Station: {from_station}\nTo Station: {to_station}\nTotal Fare: {total_fare}" 
     print(message)
     #return(message)
     
