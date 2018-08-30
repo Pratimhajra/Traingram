4904503ec2395551d268690fe733cc9d47166cb7
Unknown <mihir.prashant.kulkarni@gmail.com>
2018-08-30 20:17:04 +0530

    test2--Added formatted response to the live station function

--- test2.py
+++ test2.py
@@ -7,8 +7,11 @@ def live_status(TrainNo, stnCode):
 	date=today.strftime("%d-%b-%y")
 	response=requests.get(f"https://api.railrider.in/api_rr_v3_test.php?page_type=live_train_status&train_num={TrainNo}&journey_station={stnCode}&journey_date={date}")
 	data=response.json()
-	var=data['delay_arr']
-	print(var)
+	var=data.get('delay_arr')
+	var=var.lower()
+	ReturnStr="Train is "+var
+	return ReturnStr
 #def trains_btwn_stations(stn1, stn2, viaStn="null", trainType="ALL"):
 
-live_status(19016,'PLG')
+if __name__=="__main__":
+	live_status(19016,'PLG')
